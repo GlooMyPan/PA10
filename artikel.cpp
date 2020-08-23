448035a89bf2b7f71a3342c0b9990b81b4a7748c
GlooMy <p353180107@gmail.com>
2020-08-23 03:35:41 +0200

    finished PA10

--- artikel.cpp
+++ artikel.cpp
@@ -1,9 +1,88 @@
 #include "artikel.h"
+#include <fstream>
+#include <cmath>
+#include <iomanip>
 
-string leseArtikel ( char const dateiName[] ) {
-    return "";
+string leseArtikel(char const dateiName[]) {
+    ifstream ifs;
+    ifs.open(dateiName);
+    if (!ifs)
+        return "";
+    string rohArtikel = "", zeile;
+    while (getline(ifs, zeile)) {
+        if (zeile.empty()) {
+            rohArtikel.erase(rohArtikel.find_last_not_of(' ') + 1);
+            zeile = "\n\n";
+        } else if (zeile.back() == '-')
+            zeile.pop_back();
+        else
+            zeile += ' ';
+        rohArtikel += zeile;
+    } //end while
+    ifs.close();
+    return rohArtikel;
 }
 
-void schreibeArtikel ( string artikel, size_t breite, size_t nSpalten, size_t zwRaum ) {
-    return;
+void schreibeArtikel(string artikel, size_t breite, size_t nSpalten, size_t zwRaum) {
+    size_t spaltenBreite = (breite - (nSpalten - 1) * zwRaum) / nSpalten;
+    if (spaltenBreite < 3) {
+        cerr << "Die Spaltenbreite ist zu klein!";
+        return;
+    }
+    for (size_t e = spaltenBreite; e < artikel.size(); e += spaltenBreite) {
+        size_t a = e - spaltenBreite;
+        size_t i = artikel.find('\n', a);
+        if (i == e)
+            artikel.erase(i, 1);
+        else if (i < e)
+            artikel.replace(i, 1, e - i, ' ');
+        else if (artikel[e] == ' ')
+            artikel.erase(e, 1);
+        else {
+            i = artikel.rfind(' ', e);
+            if (i >= a)
+                artikel.replace(i, 1, e - i, ' ');
+            else
+                artikel.insert(e - 1, "-");
+        }
+
+    } // endfor
+
+    size_t gZeilen = ceil(artikel.size() / double(spaltenBreite));
+    if (artikel.size() % spaltenBreite == 0)
+        ++gZeilen;
+    size_t nZeilen = gZeilen / nSpalten;
+    if (nZeilen % nSpalten != 0 && artikel.size() % spaltenBreite != 0)
+        ++nZeilen;
+    int m = 0, n = gZeilen;
+    while (n > 0) {
+        n /= 10;
+        ++m;
+    }
+
+    cout << endl;
+    cout << "Breite der Spalten = " << setw(m) << spaltenBreite << endl;
+    cout << "Gesamzahl Zeilen   = " << setw(m) << gZeilen << endl;
+    cout << "Laenge der Spalten = " << setw(m) << nZeilen << endl;
+    cout << endl;
+
+    for (size_t i = 0; i < nZeilen; ++i) {
+        for (size_t j = i; j < gZeilen; j += nZeilen) {
+            if (j > i)
+                cout << string(zwRaum, ' ');
+//            cout << artikel.substr(j * spaltenBreite, spaltenBreite);
+            string subStr = artikel.substr(j * spaltenBreite, spaltenBreite);
+            size_t nOfSubStrWithNoSpaceAtEnd = subStr.find_last_not_of(' ') + 1, nextSpace = 0;
+            string subStr2 = subStr.substr(0, nOfSubStrWithNoSpaceAtEnd);
+            for (int k = 0; k < subStr.size() - nOfSubStrWithNoSpaceAtEnd; ++k) {
+                nextSpace = subStr2.find(' ', nextSpace);
+                if (nextSpace == string::npos)
+                    subStr2.find(' ') == string::npos ? nextSpace = 0 : nextSpace = subStr2.find(' ');
+                subStr2.insert(nextSpace, " ");
+                nextSpace += 2;
+            }
+            cout << subStr2;
+        }
+        cout << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -2,65 +2,61 @@
 #include "artikel.h"
 
 
-int main ( int narg, char* argv[] )
-{
+int main(int narg, char *argv[]) {
 /***  gebe ggf. Usage aus  ***/
 
-    if (     narg < 4								// nicht genug Argumente
-             ||  atoi( argv[ 2 ] ) < 10					// breite zu klein
-             ||  atoi( argv[ 3 ] ) <  1					// nSpalten zu klein
-             ||  (	   atoi( argv[ 3 ] ) > 1			// wenn mehr als 1 Spalte,
-                         &&  (	 narg < 5					//    muss zwRaum >= 1
-                                   ||  atoi( argv[ 4 ] ) < 1		//    gegeben sein
-                         )     )     )
-    {
-        cout << "Usage: " << argv[ 0 ]
-             << " dateiName breite nSpalten zwRaum" << endl ;
-        cout << "       dateiName: Name einer ASCII-Datei"		 << endl ;
-        cout << "       breite   : ganze Zahl >= 10"			 << endl ;
-        cout << "       nSpalten : ganze Zahl >=  1"			 << endl ;
-        cout << "       zwRaum   : ganze Zahl >=  1"			 << endl ;
-        cout << "Liest die Datei dateiName und schreibt sie auf" << endl ;
-        cout << "cout mit breite Zeichen, nSpalten Spalten mit"	 << endl ;
-        cout << "zwRaum Leerzeichen zwischen den Spalten."		 << endl ;
+    if (narg < 4                                // nicht genug Argumente
+        || atoi(argv[2]) < 10                    // breite zu klein
+        || atoi(argv[3]) < 1                    // nSpalten zu klein
+        || (atoi(argv[3]) > 1            // wenn mehr als 1 Spalte,
+            && (narg < 5                    //    muss zwRaum >= 1
+                || atoi(argv[4]) < 1        //    gegeben sein
+            ))) {
+        cout << "Usage: " << argv[0]
+             << " dateiName breite nSpalten zwRaum" << endl;
+        cout << "       dateiName: Name einer ASCII-Datei" << endl;
+        cout << "       breite   : ganze Zahl >= 10" << endl;
+        cout << "       nSpalten : ganze Zahl >=  1" << endl;
+        cout << "       zwRaum   : ganze Zahl >=  1" << endl;
+        cout << "Liest die Datei dateiName und schreibt sie auf" << endl;
+        cout << "cout mit breite Zeichen, nSpalten Spalten mit" << endl;
+        cout << "zwRaum Leerzeichen zwischen den Spalten." << endl;
 
-        return 0 ;
+        return 0;
     }
 
 /***  lese Datei in einen String ein  ***/
 
-    char const* dateiName  = argv[ 1 ] ;
-    string      rohartikel = leseArtikel( dateiName ) ;
+    char const *dateiName = argv[1];
+    string rohartikel = leseArtikel(dateiName);
 
     // rohartikel ist leer, wenn die Datei nicht gelesen werden konnte
-    if ( rohartikel.empty() )
-    {
-        cerr << "kann Datei " << dateiName << " nicht oeffnen!" << endl ;
-        return -1 ;		// willk黵licher Signalwert
+    if (rohartikel.empty()) {
+        cerr << "kann Datei " << dateiName << " nicht oeffnen!" << endl;
+        return -1;        // willk黵licher Signalwert
     }
 
 /***  gebe ganzen Artikel im Spaltensatz auf cout aus  ***/
 
     // gebe zur Kontrolle den Rohartikel aus
-    if ( rohartikel.size() <= 400 )
-    {
-        cout << "Rohartikel:" << endl ;
-        cout << string( 40, '-' ) << endl ;		// was passiert hier?
-        cout << rohartikel << endl ;
-        cout << string( 40, '-' ) << endl ;
-        cout << endl ;
+    if (rohartikel.size() <= 400) {
+        cout << "Rohartikel:" << endl;
+        cout << string(40, '-') << endl;        // was passiert hier?
+        cout << rohartikel << endl;
+        cout << string(40, '-') << endl;
+        cout << endl;
     }
 
     // lese Kommandozeilenargumente
-    size_t breite   = atoi( argv[ 2 ] ) ;
-    size_t nSpalten = atoi( argv[ 3 ] ) ;
-    size_t zwRaum   = 1 ;
-    if ( nSpalten > 1 )
-        zwRaum = atoi( argv[ 4 ] ) ;
+    size_t breite = atoi(argv[2]);
+    size_t nSpalten = atoi(argv[3]);
+    size_t zwRaum = 1;
+    if (nSpalten > 1)
+        zwRaum = atoi(argv[4]);
 
     // erzeuge Mehrspaltensatz
-    schreibeArtikel( rohartikel, breite, nSpalten, zwRaum ) ;
+    schreibeArtikel(rohartikel, breite, nSpalten, zwRaum);
 
-    return 0 ;
+    return 0;
 
 }  // main()
